40db9487523591db45a83e42257f5f2fd9b5a000
Pavel Fomkin <pavel_fomkin@epam.com>
2018-06-24 18:20:19 +0300

    Exercise2 is completed. (streams part2)

--- src/test/java/streams/part2/exercise/Exercise2.java
+++ src/test/java/streams/part2/exercise/Exercise2.java
@@ -6,6 +6,7 @@ import lambda.data.Person;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,148 +15,193 @@ public class Exercise2 {
 
     /**
      * Преобразовать список сотрудников в отображение [компания -> множество людей, когда-либо работавших в этой компании].
-     *
+     * <p>
      * Входные данные:
      * [
-     *     {
-     *         {Иван Мельников 30},
-     *         [
-     *             {2, dev, "EPAM"},
-     *             {1, dev, "google"}
-     *         ]
-     *     },
-     *     {
-     *         {Александр Дементьев 28},
-     *         [
-     *             {2, tester, "EPAM"},
-     *             {1, dev, "EPAM"},
-     *             {1, dev, "google"}
-     *         ]
-     *     },
-     *     {
-     *         {Дмитрий Осинов 40},
-     *         [
-     *             {3, QA, "yandex"},
-     *             {1, QA, "EPAM"},
-     *             {1, dev, "mail.ru"}
-     *         ]
-     *     },
-     *     {
-     *         {Анна Светличная 21},
-     *         [
-     *             {1, tester, "T-Systems"}
-     *         ]
-     *     }
+     * {
+     * {Иван Мельников 30},
+     * [
+     * {2, dev, "EPAM"},
+     * {1, dev, "google"}
      * ]
-     *
+     * },
+     * {
+     * {Александр Дементьев 28},
+     * [
+     * {2, tester, "EPAM"},
+     * {1, dev, "EPAM"},
+     * {1, dev, "google"}
+     * ]
+     * },
+     * {
+     * {Дмитрий Осинов 40},
+     * [
+     * {3, QA, "yandex"},
+     * {1, QA, "EPAM"},
+     * {1, dev, "mail.ru"}
+     * ]
+     * },
+     * {
+     * {Анна Светличная 21},
+     * [
+     * {1, tester, "T-Systems"}
+     * ]
+     * }
+     * ]
+     * <p>
      * Выходные данные:
      * [
-     *    "EPAM" -> [
-     *       {Иван Мельников 30},
-     *       {Александр Дементьев 28},
-     *       {Дмитрий Осинов 40}
-     *    ],
-     *    "google" -> [
-     *       {Иван Мельников 30},
-     *       {Александр Дементьев 28}
-     *    ],
-     *    "yandex" -> [ {Дмитрий Осинов 40} ]
-     *    "mail.ru" -> [ {Дмитрий Осинов 40} ]
-     *    "T-Systems" -> [ {Анна Светличная 21} ]
+     * "EPAM" -> [
+     * {Иван Мельников 30},
+     * {Александр Дементьев 28},
+     * {Дмитрий Осинов 40}
+     * ],
+     * "google" -> [
+     * {Иван Мельников 30},
+     * {Александр Дементьев 28}
+     * ],
+     * "yandex" -> [ {Дмитрий Осинов 40} ]
+     * "mail.ru" -> [ {Дмитрий Осинов 40} ]
+     * "T-Systems" -> [ {Анна Светличная 21} ]
      * ]
      */
+
     @Test
     public void employersStuffList() {
         List<Employee> employees = getEmployees();
 
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees.stream()
+                .flatMap(empl -> empl.getJobHistory()
+                        .stream()
+                        .map(history -> new PairEmployerPerson(history.getEmployer(), empl.getPerson())))
+                .collect(Collectors.groupingBy(PairEmployerPerson::getEmployer,
+                                            Collectors.mapping(PairEmployerPerson::getPerson, Collectors.toSet())));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("yandex", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
         expected.put("mail.ru", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
         expected.put("EPAM", new HashSet<>(Arrays.asList(
-            employees.get(0).getPerson(),
-            employees.get(1).getPerson(),
-            employees.get(4).getPerson(),
-            employees.get(5).getPerson()
+                employees.get(0).getPerson(),
+                employees.get(1).getPerson(),
+                employees.get(4).getPerson(),
+                employees.get(5).getPerson()
         )));
         expected.put("google", new HashSet<>(Arrays.asList(
-            employees.get(0).getPerson(),
-            employees.get(1).getPerson()
+                employees.get(0).getPerson(),
+                employees.get(1).getPerson()
         )));
         expected.put("T-Systems", new HashSet<>(Arrays.asList(
-            employees.get(3).getPerson(),
-            employees.get(5).getPerson()
+                employees.get(3).getPerson(),
+                employees.get(5).getPerson()
         )));
         assertEquals(expected, result);
     }
 
     /**
      * Преобразовать список сотрудников в отображение [компания -> множество людей, начавших свою карьеру в этой компании].
-     *
+     * <p>
      * Пример.
-     *
+     * <p>
      * Входные данные:
      * [
-     *     {
-     *         {Иван Мельников 30},
-     *         [
-     *             {2, dev, "EPAM"},
-     *             {1, dev, "google"}
-     *         ]
-     *     },
-     *     {
-     *         {Александр Дементьев 28},
-     *         [
-     *             {2, tester, "EPAM"},
-     *             {1, dev, "EPAM"},
-     *             {1, dev, "google"}
-     *         ]
-     *     },
-     *     {
-     *         {Дмитрий Осинов 40},
-     *         [
-     *             {3, QA, "yandex"},
-     *             {1, QA, "EPAM"},
-     *             {1, dev, "mail.ru"}
-     *         ]
-     *     },
-     *     {
-     *         {Анна Светличная 21},
-     *         [
-     *             {1, tester, "T-Systems"}
-     *         ]
-     *     }
+     * {
+     * {Иван Мельников 30},
+     * [
+     * {2, dev, "EPAM"},
+     * {1, dev, "google"}
      * ]
-     *
+     * },
+     * {
+     * {Александр Дементьев 28},
+     * [
+     * {2, tester, "EPAM"},
+     * {1, dev, "EPAM"},
+     * {1, dev, "google"}
+     * ]
+     * },
+     * {
+     * {Дмитрий Осинов 40},
+     * [
+     * {3, QA, "yandex"},
+     * {1, QA, "EPAM"},
+     * {1, dev, "mail.ru"}
+     * ]
+     * },
+     * {
+     * {Анна Светличная 21},
+     * [
+     * {1, tester, "T-Systems"}
+     * ]
+     * }
+     * ]
+     * <p>
      * Выходные данные:
      * [
-     *    "EPAM" -> [
-     *       {Иван Мельников 30},
-     *       {Александр Дементьев 28}
-     *    ],
-     *    "yandex" -> [ {Дмитрий Осинов 40} ]
-     *    "T-Systems" -> [ {Анна Светличная 21} ]
+     * "EPAM" -> [
+     * {Иван Мельников 30},
+     * {Александр Дементьев 28}
+     * ],
+     * "yandex" -> [ {Дмитрий Осинов 40} ]
+     * "T-Systems" -> [ {Анна Светличная 21} ]
      * ]
      */
     @Test
     public void indexByFirstEmployer() {
         List<Employee> employees = getEmployees();
 
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees.stream()
+                .filter(employee -> employee.getJobHistory().size() > 0)
+                .map(empl -> new PairEmployerPerson(empl.getJobHistory().get(0).getEmployer(), empl.getPerson()))
+                .collect(Collectors.groupingBy(PairEmployerPerson::getEmployer,
+                        Collectors.mapping(PairEmployerPerson::getPerson, Collectors.toSet())));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("yandex", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
         expected.put("EPAM", new HashSet<>(Arrays.asList(
-            employees.get(0).getPerson(),
-            employees.get(1).getPerson(),
-            employees.get(4).getPerson()
+                employees.get(0).getPerson(),
+                employees.get(1).getPerson(),
+                employees.get(4).getPerson()
         )));
         expected.put("T-Systems", new HashSet<>(Arrays.asList(
-            employees.get(3).getPerson(),
-            employees.get(5).getPerson()
+                employees.get(3).getPerson(),
+                employees.get(5).getPerson()
         )));
         assertEquals(expected, result);
+    }
+
+    static class TripleEmployerDurationPerson{
+        private final String employer;
+        private final Integer duration;
+        private final Person person;
+
+        public TripleEmployerDurationPerson(String employer, Integer duration, Person person) {
+
+            this.employer = employer;
+            this.duration = duration;
+            this.person = person;
+        }
+
+        String getEmployer() {
+            return employer;
+        }
+
+        public Integer getDuration() {
+            return duration;
+        }
+
+        Person getPerson() {
+            return person;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return person.toString().equals(((TripleEmployerDurationPerson) obj).getPerson().toString()) && employer.equals(((TripleEmployerDurationPerson) obj).employer);
+        }
+
+        @Override
+        public int hashCode() {
+            return person.hashCode()+employer.hashCode();
+        }
     }
 
     /**
@@ -166,7 +212,13 @@ public class Exercise2 {
     public void greatestExperiencePerEmployer() {
         List<Employee> employees = getEmployees();
 
-        Map<String, Person> collect = null;
+        Map<String, Person> collect = employees.stream()
+                .flatMap(empl -> empl.getJobHistory()
+                        .stream()
+                        .map(job -> new TrialPersonPositionDuration(empl.getPerson(), job.getEmployer(),job.getDuration())))
+                .collect(Collectors.groupingBy(TrialPersonPositionDuration::getPosition, Collectors.collectingAndThen(
+                        Collectors.maxBy(Comparator.comparingInt(TrialPersonPositionDuration::getDuration)),
+                        trial -> trial.map(TrialPersonPositionDuration::getPerson).orElseThrow(NoSuchElementException::new))));
 
         Map<String, Person> expected = new HashMap<>();
         expected.put("EPAM", employees.get(4).getPerson());
@@ -190,7 +242,7 @@ public class Exercise2 {
                         Arrays.asList(
                                 new JobHistoryEntry(1, "tester", "EPAM"),
                                 new JobHistoryEntry(2, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
+                                new JobHistoryEntry(2, "dev", "google")
                         )),
                 new Employee(
                         new Person("Дмитрий", "Осинов", 40),
@@ -218,5 +270,48 @@ public class Exercise2 {
                                 new JobHistoryEntry(1, "dev", "EPAM")
                         ))
         );
+    }
+}
+
+class PairEmployerPerson {
+    private final String employer;
+    private final Person person;
+
+    PairEmployerPerson(String employer, Person person) {
+        this.employer = employer;
+        this.person = person;
+    }
+
+    String getEmployer() {
+        return employer;
+    }
+
+    Person getPerson() {
+        return person;
+    }
+}
+
+class TrialPersonPositionDuration {
+
+    private final Person person;
+    private final String position;
+    private final int duration;
+
+    TrialPersonPositionDuration(Person person, String position, int duration) {
+        this.person = person;
+        this.position = position;
+        this.duration = duration;
+    }
+
+    Person getPerson() {
+        return person;
+    }
+
+    String getPosition() {
+        return position;
+    }
+
+    int getDuration() {
+        return duration;
     }
 }
